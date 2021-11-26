fd9b62c12426dfac0217b2a8d650b4bc4feae385
rtesagg84 <rtesagg84@gmail.com>
2021-11-26 08:26:23 -0500

    edite form  is done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,26 @@ class PagesController < ApplicationController
     def show
         @page = Page.find(params[:id])
     end
+    def new
+        @page = Page.new
+    end
+    def create
+    @page= Page.new(page_params)
+    @page.save
+    redirect_to @page
+    end
+    def edit
+        @page =Page.find(params[:id]) 
+    end
+    def update
+        @page = Page.find(params[:id]) 
+        @page.update(page_params)
+        redirect_to @page
+    end
+
+    private
+      def page_params
+        params.require(:page).permit(:title,:body)
+      end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
   get '/pages' ,to:'pages#index'
+  post '/pages' ,to:'pages#create'
+  get '/pages/new',to:'pages#new',as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
-
+  get '/pages/:id/edit',  to: 'pages#edit',as:'edit_page'
+  patch '/pages/:id', to: 'pages#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
