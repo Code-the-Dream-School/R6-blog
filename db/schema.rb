80463b5af8a690b643795872c75a227256f34d2b
rtesagg84 <rtesagg84@gmail.com>
2021-12-06 11:45:37 -0500

    creat a model comment and also insert a value to the table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_173623) do
+ActiveRecord::Schema.define(version: 2021_12_06_163448) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
