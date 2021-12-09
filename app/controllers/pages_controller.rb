3622a2cbd6f91350ba28a8fa1ad3fcf5b26b8a7a
inlibra <54969014+inlibra-cpa@users.noreply.github.com>
2021-12-09 18:22:58 -0500

    Rails Routes and Resources

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,44 @@
 class PagesController < ApplicationController
+    before_action :set_page, except: [:index, :new, :create]
+
     def index
         @pages = Page.all
     end
+    
+    def show 
+    end
+
+    def new
+        @page = Page.new
+    end
+
+    def create
+        @page = Page.new(page_params)
+        @page.save
+        redirect_to @page
+    end
+
+    def edit
+    end
+
+    def update
+        @page = Page.find(params[:id])
+        @page.update(page_params)
+        redirect_to @page
+    end
+
+    def destroy
+        @page.destroy
+        redirect_to pages_path
+    end
+
+    private  
+
+      def page_params
+          params.require(:page).permit(:title, :body, :slug)
+      end
+
+      def set_page
+        @page = Page.find(params[:id])
+      end
 end
