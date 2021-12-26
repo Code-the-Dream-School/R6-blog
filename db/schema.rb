31ef954ccc9725293240caad1d14aff2e29003dd
inlibra <54969014+inlibra-cpa@users.noreply.github.com>
2021-12-26 17:54:01 -0500

    Active Rails Associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_024455) do
+ActiveRecord::Schema.define(version: 2021_12_16_234605) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
