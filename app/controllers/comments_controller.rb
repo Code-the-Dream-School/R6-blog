57f67fa94b6e56c902d47522f77352fb85d0785a
inlibra <54969014+inlibra-cpa@users.noreply.github.com>
2022-01-14 21:01:58 -0500

    Active Rails Associatios last drop

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,36 @@
-def create
-    @post = Post.find(params[:post_id])
+class CommentsController < ApplicationController
+  before_action :set_post
+  # No `new` action because form is provided by PostsController#show
+  def create
+    # Create associated model, just like we did in the console before
     @comment = @post.comments.new(comment_params)
+    # We want to show the comment in the context of the Post
+    redirect_to @post
+  end
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @post, notice: 'Comment was successfully created.' } # changed the redirect to @post
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to @post
+  end
+
+  def destroy
+    @comment = Comment.find(params [:id])
+    @comment.destroy
+    redirect_to @post
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:name, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+end
