09a005bc269addb84a0eb2a0c5a095ed890e432c
Cowboy <cbrobak79@gmail.com>
2023-01-02 09:36:06 -0500

    Week 6 assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_30_034942) do
+ActiveRecord::Schema.define(version: 2022_12_30_060600) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
