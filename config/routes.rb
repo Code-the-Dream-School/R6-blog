8bda34286e77667cc317e387d7e12cc2afbe0dc8
Cowboy <cbrobak79@gmail.com>
2023-01-11 18:17:33 -0500

    Added CRUD operations to associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+    # get    '/comments',          to: 'comments#index', as: 'comments'
+    # post   '/comments',          to: 'comments#create'
+    # get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
+    # get    '/comments/:id',      to: 'comments#show',  as: 'comment'
+    # get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
+    # patch  '/comments/:id',      to: 'comments#update'
+    # delete '/comments/:id',      to: 'comments#destroy'
+  end
   resources :pages
+
+ 
   # get "/pages", to: "pages#index"
   # post "/pages", to: "pages#create"
   # get "/pages/new", to: "pages#new", as: "new_page"
